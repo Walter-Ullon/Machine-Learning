f41ae481c1c22d4e4ed46e4d7261c74332498352
Walter Ullon <wullon@aol.com>
2017-07-24 07:12:20 -0400

    finalized MushroomsClassifier.R

--- DecisionTrees/MushroomsClassifier.R
+++ DecisionTrees/MushroomsClassifier.R
@@ -1,3 +1,5 @@
+# Mushroom classification of edible vs. poisonous. 
+# Dataset downloaded from Kaggle.com
 
 install.packages("C50")
 library(C50)
@@ -17,4 +19,34 @@ str(shrooms)
 
 # Distribution of edible vs poisonous shrooms
 table(shrooms$class)
+
+# Obtain random sample to split between training and test data. Set seed.
+# Returns a vector with 900 random values
+set.seed(7)
+train_sample <- sample(8124, 7124)
+str(train_sample)
+
+# Split data into training and test sets.
+# Check to see if both sets have ~ proportion of edible vs poisonous.
+shrooms_train <- shrooms[train_sample, ]
+shrooms_test <- shrooms[-train_sample, ]
+prop.table(table(shrooms_train$class))
+prop.table(table(shrooms_test$class))
+
+# remove veil.type variable (17th col.) as it only contain one value "p" and tells us nothing.
+shrooms_train <- shrooms_train[-17]
+shrooms_test <- shrooms_test[-17]
+
+# Train model.
+# Exclude "class" class (edible, posonous) from training data (1st column) but supply it as target factor vector for classification (labels).
+shrooms_model <- C5.0(shrooms_train[ ,-1], shrooms_train$class)
+summary(shrooms_model)
+
+# Apply model to test data and predict...
+# Compare prediction against true results.
+shrooms_pred <- predict(shrooms_model, shrooms_test)
+CrossTable(shrooms_test$class, shrooms_pred, prop.chisq = FALSE, prop.r = FALSE, dnn = c("actual", "predicted" ))
+
+
+# MORAL OF THE STORY: always smell the mushrooms!!!
 
